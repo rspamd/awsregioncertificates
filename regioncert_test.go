f92940f01b81b66e1d955d64e8e06df59e3fc4e0
Andrew Lewis <nerf@judo.za.org>
2025-05-09 00:19:34 +0200

    Add example to documentation

--- regioncert_test.go
+++ regioncert_test.go
@@ -1,6 +1,7 @@
 package awsregioncertificates
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path"
@@ -14,6 +15,42 @@ var (
 		[3]string{"iid1.json", "iid1.sig", "us-east-1"}: nil,
 	}
 )
+
+func ExampleRegionKeys_ValidateID() {
+	regionKeys, err := GetRegionKeys()
+	if err != nil {
+		fmt.Println(fmt.Sprintf("error loading region keys: %v", err))
+		return
+	}
+	iid := []byte(`{
+  "accountId" : "975050371289",
+  "architecture" : "x86_64",
+  "availabilityZone" : "us-east-1b",
+  "billingProducts" : null,
+  "devpayProductCodes" : null,
+  "marketplaceProductCodes" : null,
+  "imageId" : "ami-0c7217cdde317cfec",
+  "instanceId" : "i-0b02d936754a6d637",
+  "instanceType" : "t2.micro",
+  "kernelId" : null,
+  "pendingTime" : "2024-02-15T14:12:11Z",
+  "privateIp" : "172.31.17.154",
+  "ramdiskId" : null,
+  "region" : "us-east-1",
+  "version" : "2017-09-30"
+}`)
+	iidSig := []byte(`OQTgfPTsc7hXR+3OWP7dk7qY1S1RNGvVvoVPzn/WogAqJpGBtei2pSx3OfZ7F1PDMpClQswDcF9N
+iZmPD09xyJSrSRwYvx8SFoBWzUXS1hd9T1ZxpqWtloe/k//YmK7h9f7rjuT3/CxDDCWrbsKp8F0N
+ck+YPKGzD+dtxEm6g1g=`)
+	err = regionKeys.ValidateID("us-east-1", iid, iidSig)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("error validating instance ID: %v", err))
+		return
+	}
+	fmt.Println("validated OK")
+	// Output:
+	// validated OK
+}
 
 func TestValidateID(t *testing.T) {
 	regionKeys, err := GetRegionKeys()
